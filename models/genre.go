698640b2593bec422b664be9739a236e4e9d404c
Aliffio Syah Maulana <67381316+alif-syhm01@users.noreply.github.com>
2023-10-14 15:45:58 +0700

    Update genre.go

--- models/genre.go
+++ models/genre.go
@@ -1,6 +1,6 @@
 package models
 
 type Genre struct {
-	ID   uint   `gorm:"primary_key" json:"id" form:"name"`
-	Name string `gorm:"not null" json:"name" form:"name"`
+	ID   uint   `gorm:"primary_key" json:"id" form:"id"`
+	Name string `gorm:"not null" json:"name" form:"name" validate:"required"`
 }
